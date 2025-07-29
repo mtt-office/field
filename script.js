b47fd9edee7473b6e29e2cf2e005f0ee63a9dd5a
peuvxx <choisubin031210@gmail.com>
2025-07-30 02:31:07 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const ball = document.getElementById("ball");
+let x = 100;
+let y = 100;
+let vx = 10;
+let vy = 10;
+
+function updatePosition() {
+  const containerWidth = window.innerWidth;
+  const containerHeight = window.innerHeight;
+  const ballWidth = ball.offsetWidth;
+  const ballHeight = ball.offsetHeight;
+
+  x += vx;
+  y += vy;
+
+  if (x + ballWidth >= containerWidth || x <= 0) vx *= -1;
+  if (y + ballHeight >= containerHeight || y <= 0) vy *= -1;
+
+  ball.style.left = x + "px";
+  ball.style.top = y + "px";
+
+  requestAnimationFrame(updatePosition);
+}
+
+window.onload = () => {
+  ball.style.left = x + "px";
+  ball.style.top = y + "px";
+  updatePosition();
+};
+
+window.onresize = () => {
+  const maxX = window.innerWidth - ball.offsetWidth;
+  const maxY = window.innerHeight - ball.offsetHeight;
+  if (x > maxX) x = maxX;
+  if (y > maxY) y = maxY;
+};
